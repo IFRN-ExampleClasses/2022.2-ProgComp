239e5b78ad5d6a3198ae9f98d325baac27597628
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2022-12-26 21:51:32 -0300

    Atualização de Repositório

---
+++ 2022_12_26/exemplo_03.py
@@ -0,0 +1,14 @@
+eixo_x = [x for x in range(1, 11)]
+
+# equacao da reta
+# y = 2 * x + 3
+
+eixo_y = [2*x+3 for x in eixo_x]
+
+'''
+for x in eixo_x:
+   eixo_y.append(2*x+3)
+'''
+
+print(eixo_x)
+print(eixo_y)
